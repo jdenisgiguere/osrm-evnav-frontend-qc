de1b417780f7ca0f6dd444809f91d42a8d868090
Jean-Denis Giguere <jdenisgiguere@gmail.com>
2015-12-04 14:17:54 -0500

    Marqueur circuit électrique

--- index.js
+++ index.js
@@ -6,8 +6,31 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 
 L.Routing.control({
     waypoints: [
-        L.latLng(45.487851, -75.70122),
-        L.latLng(45.587851, -75.80122)
+        L.latLng(45.487851, -75.50122),
+        L.latLng(45.587851, -75.60122)
     ],
     routeWhileDragging: true
 }).addTo(map);
+
+var stationsGeojson = { "type": "FeatureCollection",
+    "features" : [
+        { "type": "Feature",
+            "geometry": {"type": "Point", "coordinates": [-75.70122, 45.487851]},
+            "properties" : { "voltage" : 240 }
+        },
+    ]
+}
+
+var stationMarkerOptions = {
+    icon : L.divIcon({
+        html : '<div class="map-marker-icon map-marker-icon-legend-CE"></div>'
+    })
+};
+
+L.geoJson(stationsGeojson, {
+    pointToLayer : function(feature, latlng) {
+        return L.marker(latlng, stationMarkerOptions);
+    }
+}).addTo(map);
+
+
