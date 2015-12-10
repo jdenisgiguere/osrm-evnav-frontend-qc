d8b7bf8bdd7481d1d0e91819506d46ff45af91b7
Jean-Denis Giguere <jdenisgiguere@gmail.com>
2015-12-10 11:43:54 -0500

    Loading all stations from CE

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+
+
 var map = L.map('map');
 
 L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
+
 L.Routing.control({
+    router: L.Routing.osrm({
+            serviceUrl:  'http://router.project-osrm.org/viaroute'
+    }),
     waypoints: [
         L.latLng(45.487851, -75.50122),
         L.latLng(45.587851, -75.60122)
@@ -19,8 +25,21 @@ var stationMarkerOptions = {
     })
 };
 
-L.geoJson.ajax('station.json', {
-    pointToLayer : function(feature, latlng) {
+L.geoJson.ajax('stations-ce.json', {
+    middleware: function(stationsCE) {
+        var stationsGeoJSON = { "type": "FeatureCollection",
+            "features" : []
+        };
+        for (i = 0; i < stationsCE.length; i++) {
+            var ce = stationsCE[i];
+            stationsGeoJSON.features.push({ "type": "Feature",
+                "geometry": {"type": "Point", "coordinates": [ce.LatLng.Lng, ce.LatLng.Lat]},
+                "properties" : { "emplacement" : ce.ParkName }
+            });
+        }
+        return stationsGeoJSON;
+    },
+    pointToLayer: function(feature, latlng) {
         return L.marker(latlng, stationMarkerOptions);
     }
 }).addTo(map);
